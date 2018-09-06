8de5d0a83529fa08def8d768bf7c28b81971690c
Scott Henderson <henders999@gmail.com>
2018-09-05 20:17:24 -0700

    Changed video processing from OpenCV to MoviePy.

--- extract_video_frames.py
+++ extract_video_frames.py
@@ -8,6 +8,8 @@ Created on Fri Aug 31 18:26:47 2018
 import os
 import cv2
 
+from moviepy.editor import VideoFileClip
+
 
 # Image text parameters
 TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
@@ -17,28 +19,57 @@ TEXT_THICKNESS = 2
 TEXT_LINE_TYPE = cv2.LINE_AA
 
 
-video_file = './project_video.mp4'
-output_dir = './project_video_frames'
+class WriteFrame:
+    def __init__(self):
+        # Current frame number.
+        self.current_frame = 0
+        # Output dir for modified video frames.
+        self.output_dir = None
+
+    def write_frame(self, get_frame, t):
+        self.current_frame += 1
+
+        img = get_frame(t)
+
+        # Write the frame number to the image.
+        frame = 'Frame: {}'.format(self.current_frame)
+        cv2.putText(img, frame, (1050, 30),
+                    TEXT_FONT, TEXT_SCALE, TEXT_COLOR, TEXT_THICKNESS, TEXT_LINE_TYPE)
+
+        # Write the time (parameter t) to the image.
+        time = 'Time: {}'.format(int(round(t)))
+        cv2.putText(img, time, (1050, 700),
+                    TEXT_FONT, TEXT_SCALE, TEXT_COLOR, TEXT_THICKNESS, TEXT_LINE_TYPE)
+
+        if self.output_dir is not None:
+            output_file = os.path.join(self.output_dir,
+                                       'frame{:06d}.jpg'.format(self.current_frame))
+            filled_lane_cv2 = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(output_file, filled_lane_cv2)
+
+        # Return the modified image.
+        return img
 
 
-vidcap = cv2.VideoCapture(video_file)
-if not vidcap.isOpened():
-    print('Unable to open video file {}'.format(video_file))
-else:
-    current_frame = 0
-    while vidcap.isOpened():
-        # Read a frame from the video file.
-        ret, img = vidcap.read()
-        if ret:
-            current_frame += 1
-            print('Frame {}'.format(current_frame))
+wf = WriteFrame()
+wf.output_dir = './project_video_frames'
 
-            # Write the frame number to the image.
-            frame = 'Frame: {}'.format(current_frame)
-            cv2.putText(img, frame, (1050, 30),
-                        TEXT_FONT, TEXT_SCALE, TEXT_COLOR, TEXT_THICKNESS, TEXT_LINE_TYPE)
+current_frame = 0
+input_file = './project_video.mp4'
+input_clip = VideoFileClip(input_file)
 
-            # Save the frame image to a file.
-            cv2.imwrite(os.path.join(output_dir, 'frame{:06d}.jpg'.format(current_frame)), img)
-        else:
-            break
+output_clip = input_clip.fl(wf.write_frame)
+
+# Save the resulting, modified, video clip to a file.
+head, ext = os.path.splitext(input_file)
+root, file_name = os.path.split(head)
+output_file = os.path.join(wf.output_dir, file_name + '_lanes' + ext)
+output_clip.write_videofile(output_file, audio=False)
+
+# Cleanup
+input_clip.reader.close()
+input_clip.audio.reader.close_proc()
+del input_clip
+output_clip.reader.close()
+output_clip.audio.reader.close_proc()
+del output_clip
