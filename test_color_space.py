fe2ab14aec187fac62ccec6305a40a86cdfe58f9
BScottHenderson <henders999@gmail.com>
2019-09-05 17:30:33 -0700

    Significant code cleanup and formatting.

--- test_color_space.py
+++ test_color_space.py
@@ -13,54 +13,41 @@ import numpy as np
 import cv2
 
 
+# Color and gradient thresholds.
+COLOR_THRESHOLD = (220, 255) #(180, 230)    # (170, 250) (150, 230)
+GRADIENT_THRESHOLD = (65, 130)  # (65, 100) (65, 130)
+
+
 def color_gradient_threshold(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
-    img = np.copy(img)
+    """
+    Apply a color threshold and a gradient threshold to the given image.
 
-    # Convert to HLS color space and separate the channels.
-    lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-    L = lab[:, :, 0]
-#    A = lab[:, :, 1]
-    B = lab[:, :, 2]
+    Args:
+        img: apply thresholds to this image
+        s_thresh: Color threshold (apply to S channel of HSV)
+        sx_thresh: Gradient threshold (apply to x gradient on L channel of HSV)
 
-#    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-#    H = hls[:, :, 0]
-#    L = hls[:, :, 1]
-#    S = hls[:, :, 2]
+    Returns:
+        new image with thresholds applied
+    """
 
-    # Apply Sobel x to the L channel for HLS, LAB
-    # use V channel for HSV
-    sobelx = cv2.Sobel(L, cv2.CV_64F, 1, 0)  # Take the derivative in x
-    # Absolute x derivative to accentuate lines away from horizontal
-    abs_sobelx = np.absolute(sobelx)
-    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
-
-    # Appply gradient threshold.
-    sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= sx_thresh[0]) &
-             (scaled_sobel <= sx_thresh[1])] = 1
-
-    # Apply color channel threshold
-    s_binary = np.zeros_like(B)
-    s_binary[(B >= s_thresh[0]) & (B <= s_thresh[1])] = 1
-
-    # Stack each channel and return.
-    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary))
-    color_binary *= 255  # Convert from [0, 1] back to [0, 255]
-    return color_binary
-
-
-def sobel(img, sx_thresh=(20, 100)):
     img = np.copy(img)
 
     # Convert to HLS color space and separate the channels.
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-#    H = hls[:, :, 0]
-    L = hls[:, :, 1]
+    # H = hls[:, :, 0]
+    # L = hls[:, :, 1]
     S = hls[:, :, 2]
 
-    # Apply Sobel x to the L channel
-    sobelx = cv2.Sobel(L, cv2.CV_64F, 1, 0)  # Take the derivative in x
-    # Absolute x derivative to accentuate lines away from horizontal
+    # Convert to LAB color space and separate the channels.
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    L = lab[:, :, 0]
+    # A = lab[:, :, 1]
+    B = lab[:, :, 2]
+
+    # Apply Sobel x (take the derivative on the x axis) to the HLS L channel.
+    sobelx = cv2.Sobel(L, cv2.CV_64F, 1, 0)
+    # Absolute x derivative to accentuate lines away from horizontal.
     abs_sobelx = np.absolute(sobelx)
     scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
 
@@ -69,78 +56,110 @@ def sobel(img, sx_thresh=(20, 100)):
     sxbinary[(scaled_sobel >= sx_thresh[0]) &
              (scaled_sobel <= sx_thresh[1])] = 1
 
+    # Apply color channel threshold.
+    s_thresh = (125, 180)
+    S = S * (255 / np.max(S))  # normalize
+    S_thresh = np.zeros_like(S)
+    S_thresh[(S > s_thresh[0]) & (S <= s_thresh[1])] = 1
+
+    s_thresh = (220, 255)
+    B = B * (255 / np.max(B))  # normalize
+    B_thresh = np.zeros_like(B)
+    B_thresh[(B > s_thresh[0]) & (B <= s_thresh[1])] = 1
+
+    # Combine HLS S and Lab B channel thresholds.
+    sb_binary = np.zeros_like(S_thresh)
+    sb_binary[(S_thresh == 1) | (B_thresh == 1)] = 1
+
     # Stack each channel and return.
-#    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary))
-#    color_binary *= 255  # Convert from [0, 1] back to [0, 255]
-    color_binary = sxbinary * 255
-    return color_binary
+    #                         B                        G         R
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, sb_binary))
+    color_binary *= 255  # Convert from [0, 1] back to [0, 255]
+    return np.uint8(color_binary)
 
 
+def main(name=None):
 
-image_dir = './test_frames'
-output_dir = './test_frames_output'
+    print('Name: {}\n'.format(name))
 
-images = glob.glob(os.path.join(image_dir, '*.jpg'))
-for fname in images:
-    print('Processing image {}'.format(fname))
-    _, name = os.path.split(fname)
-    name, ext = os.path.splitext(name)
+    image_dir = './test_frames'
+    output_dir = './test_frames_output'
 
-    # Read the image.
-    img = cv2.imread(fname)
-    img_size = (img.shape[1], img.shape[0])
+    images = glob.glob(os.path.join(image_dir, '*.jpg'))
+    for fname in images:
+        print('Processing image {} ...'.format(fname))
+        _, name = os.path.split(fname)
+        name, ext = os.path.splitext(name)
 
-    # Color/gradient threshold
-#    thresh = color_gradient_threshold(img,
-#                                      s_thresh=COLOR_THRESHOLD,
-#                                      sx_thresh=GRADIENT_THRESHOLD)
-#    cv2.imwrite(os.path.join(output_dir, name + '_2_threshold') + ext, thresh)
+        # Read the image.
+        img = cv2.imread(fname)
+        # img_size = (img.shape[1], img.shape[0])
 
-#    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-#    H = hls[:, :, 0]
-#    L = hls[:, :, 1]
-#    S = hls[:, :, 2] #
-#    cv2.imwrite(os.path.join(output_dir, name + '_hls') + ext, hls)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hls_h') + ext, H)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hls_l') + ext, L)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hls_s') + ext, S)
+        # Color/gradient threshold
+        thresh = color_gradient_threshold(img,
+                                          s_thresh=COLOR_THRESHOLD,
+                                          sx_thresh=GRADIENT_THRESHOLD)
+        cv2.imwrite(os.path.join(output_dir, name + '_color_gradient_threshold') + ext, thresh)
 
-#    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hsv') + ext, hsv)
-#    H = hsv[:, :, 0]
-#    S = hsv[:, :, 1] #
-#    V = hsv[:, :, 2]
-#    cv2.imwrite(os.path.join(output_dir, name + '_hsv') + ext, hsv)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hsv_h') + ext, H)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hsv_s') + ext, S)
-#    cv2.imwrite(os.path.join(output_dir, name + '_hsv_v') + ext, V)
+        # hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+        # H = hls[:, :, 0] # left line is clear, dashed line is ok
+        # L = hls[:, :, 1]
+        # S = hls[:, :, 2] # left line is clear, dashed line is ok
+        # cv2.imwrite(os.path.join(output_dir, name + '_hls') + ext, hls)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hls_h') + ext, H)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hls_l') + ext, L)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hls_s') + ext, S)
 
-#    yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-#    cv2.imwrite(os.path.join(output_dir, name + '_yuv') + ext, yuv)
-#    Y = yuv[:, :, 0]
-#    U = yuv[:, :, 1]
-#    V = yuv[:, :, 2] #
-#    cv2.imwrite(os.path.join(output_dir, name + '_yuv') + ext, yuv)
-#    cv2.imwrite(os.path.join(output_dir, name + '_yuv_y') + ext, Y)
-#    cv2.imwrite(os.path.join(output_dir, name + '_yuv_u') + ext, U)
-#    cv2.imwrite(os.path.join(output_dir, name + '_yuv_v') + ext, V)
+        # Use HLS, S channel to detect dashed white lane lines.
+        # s_thresh = (125, 180)
+        # S = S * (255 / np.max(S))  # normalize
+        # S_thresh = np.zeros_like(S)
+        # S_thresh[(S > s_thresh[0]) & (S <= s_thresh[1])] = 1
 
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    cv2.imwrite(os.path.join(output_dir, name + '_lab') + ext, lab)
-    L = lab[:, :, 0]
-    A = lab[:, :, 1]
-    B = lab[:, :, 2] #
-    cv2.imwrite(os.path.join(output_dir, name + '_lab') + ext, lab)
-    cv2.imwrite(os.path.join(output_dir, name + '_lab_l') + ext, L)
-    cv2.imwrite(os.path.join(output_dir, name + '_lab_a') + ext, A)
-    cv2.imwrite(os.path.join(output_dir, name + '_lab_b') + ext, B)
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hsv') + ext, hsv)
+        # H = hsv[:, :, 0] # left line is clear, dashed line is ok
+        # S = hsv[:, :, 1] # left line is clear, dashed line is gone
+        # V = hsv[:, :, 2]
+        # cv2.imwrite(os.path.join(output_dir, name + '_hsv') + ext, hsv)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hsv_h') + ext, H)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hsv_s') + ext, S)
+        # cv2.imwrite(os.path.join(output_dir, name + '_hsv_v') + ext, V)
 
-#    s_binary = np.zeros_like(B)
-#    s_binary[(B >= 50) & (B <= 150)] = 1
-#    color_binary = s_binary * 255  # Convert from [0, 1] back to [0, 255]
-#    cv2.imwrite(os.path.join(output_dir, name + '_lab_b_thresh') + ext, color_binary)
+        # yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+        # cv2.imwrite(os.path.join(output_dir, name + '_yuv') + ext, yuv)
+        # Y = yuv[:, :, 0]
+        # U = yuv[:, :, 1] # left line is clear, dashed line is very dim
+        # V = yuv[:, :, 2] # left line is clear, dashed line is gone
+        # cv2.imwrite(os.path.join(output_dir, name + '_yuv') + ext, yuv)
+        # cv2.imwrite(os.path.join(output_dir, name + '_yuv_y') + ext, Y)
+        # cv2.imwrite(os.path.join(output_dir, name + '_yuv_u') + ext, U)
+        # cv2.imwrite(os.path.join(output_dir, name + '_yuv_v') + ext, V)
 
-#    thresh = color_gradient_threshold(img,
-#                                      s_thresh=(110, 150),
-#                                      sx_thresh=(60, 100))
-#    cv2.imwrite(os.path.join(output_dir, name + '_thresh') + ext, thresh)
+        # lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        # L = lab[:, :, 0]
+        # A = lab[:, :, 1] # nothing
+        # B = lab[:, :, 2] # left line is clear, dashed line is very dim
+        # cv2.imwrite(os.path.join(output_dir, name + '_lab') + ext, lab)
+        # cv2.imwrite(os.path.join(output_dir, name + '_lab_l') + ext, L)
+        # cv2.imwrite(os.path.join(output_dir, name + '_lab_a') + ext, A)
+        # cv2.imwrite(os.path.join(output_dir, name + '_lab_b') + ext, B)
+
+
+        # s_thresh = (220, 255)
+        # B = B * (255 / np.max(B))  # normalize
+        # B_thresh = np.zeros_like(B)
+        # B_thresh[((B > s_thresh[0]) & (B <= s_thresh[1]))] = 1
+
+        # sb_binary = np.zeros_like(S_thresh)
+        # sb_binary[(S_thresh == 1) | (B_thresh == 1)] = 1
+
+        # #                         B                         G         R
+        # color_binary = np.dstack((np.zeros_like(sb_binary), S_thresh, B_thresh))
+        # color_binary *= 255  # Convert from [0, 1] back to [0, 255]
+        # color_binary = np.uint8(color_binary)
+        # cv2.imwrite(os.path.join(output_dir, name + '_color_threshold') + ext, color_binary)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
